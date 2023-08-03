3ea0d8fc11ffb4d4fba2fbdb9374be690d651f42
WillianGust <williandes.silva@yahoo.ca>
2023-08-03 14:03:29 -0400

    Creation of cards with projects, making actions JS and CSS

--- script.js
+++ script.js
@@ -72,3 +72,71 @@ function sobreMim() {
   });
 }
 sobreMim();
+
+const listaAll = document.querySelectorAll('.projects_armazenamento ul li');
+const buttonGeral = document.querySelectorAll('.projects_models ul li');
+const buttonAll = document.querySelectorAll('.projects_models .all');
+
+function removeClick(index){
+  buttonGeral.forEach((item)=>{
+    item.classList.remove('ativo');
+  });
+  buttonGeral[index].classList.add('ativo');
+}
+
+buttonGeral.forEach((item, index)=>{
+  item.addEventListener('click', ()=>{
+    removeClick(index);
+  })
+})
+
+function showLista(lista, buttom = "all"){
+  lista.forEach((item)=>{
+    item.classList.remove('ativo');
+  });
+  if(buttom == 'design'){
+    lista[0].classList.add('ativo');
+    lista[1].classList.add('ativo');
+  }
+  if(buttom == 'graphic'){
+    lista[2].classList.add('ativo');
+    lista[3].classList.add('ativo');
+  }
+  if(buttom == 'website'){
+    lista[4].classList.add('ativo');
+    lista[5].classList.add('ativo');
+    lista[6].classList.add('ativo');
+    lista[7].classList.add('ativo');
+  }
+  if(buttom == 'all'){
+    lista[0].classList.add('ativo');
+    lista[1].classList.add('ativo');
+    lista[2].classList.add('ativo');
+    lista[3].classList.add('ativo');
+    lista[4].classList.add('ativo');
+    lista[5].classList.add('ativo');
+    lista[6].classList.add('ativo');
+    lista[7].classList.add('ativo');
+  }
+}
+
+buttonGeral.forEach((item)=>{
+  item.addEventListener('click', (e)=>{
+    let currentButton = e.target;
+    if(currentButton.classList.contains('all')){
+      showLista(listaAll);
+    }
+    if(currentButton.classList.contains('design')){
+      showLista(listaAll, "design");
+    }
+    if(currentButton.classList.contains('graphic')){
+      showLista(listaAll, "graphic");
+    }
+    if(currentButton.classList.contains('website')){
+      showLista(listaAll, "website");
+    }
+    if(currentButton.classList.contains('all')){
+      showLista(listaAll, "all");
+    }
+  })
+});
